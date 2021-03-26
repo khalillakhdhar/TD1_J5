885bcaaa4278756887003306d4ed40eb2ee4b738
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-26 11:25:24 +0100

    Update HomeController.java

--- src/main/java/com/projet/TD1_J5/controller/HomeController.java
+++ src/main/java/com/projet/TD1_J5/controller/HomeController.java
@@ -2,9 +2,11 @@ package com.projet.TD1_J5.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,4 +24,19 @@ public class HomeController {
 		
 		return "page2";
 	}
+	@RequestMapping(value = "evaluation",method = RequestMethod.GET)
+	public String getForm()
+	{
+		return "Employee";
+		
+	}
+	@RequestMapping(value = "evaluation", method = RequestMethod.POST)
+	public String evaluate(Model m, HttpServletRequest req)
+	{
+		String nom=req.getParameter("nom");
+		String salaire=req.getParameter("salaire");
+		
+		return "Employee";
+	}
+	
 }
