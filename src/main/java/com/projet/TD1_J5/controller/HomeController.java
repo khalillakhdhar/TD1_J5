68e8f0c08cfb605c61702e29e6cea5f90692962e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-26 11:13:25 +0100

    Update HomeController.java

--- src/main/java/com/projet/TD1_J5/controller/HomeController.java
+++ src/main/java/com/projet/TD1_J5/controller/HomeController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,5 +15,11 @@ public class HomeController {
 	@RequestMapping(value="/")
 	public ModelAndView test(HttpServletResponse response) throws IOException{
 		return new ModelAndView("home");
+	}
+	@RequestMapping(value = "p2",method = RequestMethod.GET)
+	public String showpage()
+	{
+		
+		return "page2";
 	}
 }
